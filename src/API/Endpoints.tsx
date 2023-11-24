1396f2a99dfdc658fe7b58f4f9a96c95c2ea3f72
Dc Valerian <Dc-Valerian>
2023-11-24 11:54:16 +0100

    created and added the userHome

---
+++ src/API/Endpoints.tsx
@@ -0,0 +1,105 @@
+import axios from "axios";
+import { UserDetails } from "../AllInterface/Allinterface";
+
+const config = {
+  headers: {
+    "Content-Type": "Application/json",
+  },
+};
+
+const URl = "https://giftcard-api.onrender.com";
+
+export const createUser = async ({
+  name,
+  email,
+  username,
+  password,
+  confirmPassword,
+  phoneNumber,
+}: any) => {
+  return await axios
+    .post(`${URl}/api/registeruser`, {
+      name,
+      email,
+      password,
+      confirmPassword,
+      phoneNumber,
+      username,
+    })
+    .then((res) => {
+      return res.data;
+    });
+};
+export const createBizUser = async ({
+  name,
+  email,
+  password,
+  confirmPassword,
+  phoneNumber,
+}: any) => {
+  return await axios
+    .post(`${URl}/api/registerbusiness`, {
+      name,
+      email,
+      password,
+      confirmPassword,
+      phoneNumber,
+    })
+    .then((res) => {
+      return res.data;
+    });
+};
+
+export const LoginBizUser = async ({ email, password }: any) => {
+  return await axios
+    .post(`${URl}/api/loginbusiness`, {
+      email,
+      password,
+    })
+    .then((res) => {
+      return res.data;
+    });
+};
+export const LoginUser = async ({ email, password }: any) => {
+  return await axios
+    .post(`${URl}/api/loginuser`, {
+      email,
+      password,
+    })
+    .then((res) => {
+      return res.data;
+    });
+};
+
+export const getOneBiz = async ({ id }: any) => {
+  return axios.get(`${URl}/api/getsinglebusiness/${id}`).then((res) => {
+    return res.data;
+  });
+};
+
+export const createGiftCard = async ({ colour, moneyWorth, id }: any) => {
+  return axios.post(
+    `${URl}/api/generateyourgiftcard/${id}`,
+    {
+      colour,
+      moneyWorth,
+    },
+    config
+  );
+};
+
+export const allGiftCard = async () => {
+  return axios.get(`${URl}/api/getallgiftcards`);
+};
+
+export const singleBusiness = async (id: any) => {
+  return await axios.get(`${URl}/api/getsinglebusiness/${id}/cards`);
+};
+
+export const singleGiftCard = async (id: any) => {
+  return axios.get(`${URl}/api/businessgiftcard/${id}`);
+};
+
+export const transactionHistory = async (id: any) => {
+  return axios.post(`${URl}//fundwallet/${id}`);
+};
---
+++ src/pages/DashBoardPages/UserHome/ViewCards.tsx
@@ -0,0 +1,104 @@
+import styled from "styled-components";
+import pic from "../../../Assets/trancard.svg";
+import { Link } from "react-router-dom";
+import { allGiftCard } from "../../../API/Endpoint";
+import { useQuery } from "@tanstack/react-query";
+import Card from "./Card";
+
+const ViewCards = () => {
+  const AllgiftCards = useQuery({
+    queryKey: ["Allgiftcards"],
+    queryFn: allGiftCard,
+  });
+
+  console.log("bus", AllgiftCards.data);
+
+  return (
+    <div>
+      {AllgiftCards?.data?.data.length < 0 ? (
+        <Nothing>
+          <CenterHold>
+            <Pic>
+              <img src={pic} />
+            </Pic>
+            <Txt>No Withdrawals Yet!</Txt>
+            <Sxt>
+              This place is empty because you haven't made any withdrawal 😒
+            </Sxt>
+          </CenterHold>
+        </Nothing>
+      ) : (
+        <div>
+          <CardContainer>
+            {AllgiftCards?.data?.data?.data?.map((props: any) => (
+              <Link
+                style={{ textDecoration: "none" }}
+                to={`/user-dashboard/card/${props?._id}`}
+              >
+                <Card
+                  pic={props?.BrandLogo}
+                  busyname={props?.name}
+                  amount={props?.moneyWorth}
+                  colour={props?.colour}
+                  code={props?.uniqueID}
+                />
+              </Link>
+            ))}
+          </CardContainer>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ViewCards;
+
+const Container = styled.div`
+  /* overflow: scroll; */
+`;
+const CardContainer = styled.div`
+  width: 100%;
+  margin-top: 30px;
+  display: flex;
+  justify-content: center;
+  gap: 40px;
+  flex-wrap: wrap;
+`;
+const Nothing = styled.div`
+  width: 100%;
+  height: calc(100vh - 205px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const CenterHold = styled.div`
+  color: lightgray;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+const Pic = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+  width: 200px;
+  height: 200px;
+  img {
+    height: 100%;
+    width: 100%;
+    object-fit: contain;
+    object-position: center;
+    border-radius: 50%;
+  }
+`;
+const Txt = styled.div`
+  font-size: 16px;
+  color: gray;
+  font-weight: bold;
+`;
+const Sxt = styled.div`
+  font-size: 14px;
+  max-width: 250px;
+  color: #9c9c9c;
+`;
